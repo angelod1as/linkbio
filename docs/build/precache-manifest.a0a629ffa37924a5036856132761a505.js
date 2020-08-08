dfa03af1183b24e4ee504c6e16b71628f2e7d764
Angelo Dias <oiangelodias@gmail.com>
2020-08-08 02:36:15 -0300

    build

---
+++ docs/build/precache-manifest.a0a629ffa37924a5036856132761a505.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cd3969415a811909658f4bb8b3af69",
+    "url": "/linkbio/index.html"
+  },
+  {
+    "revision": "daf69e2651172c308950",
+    "url": "/linkbio/static/js/2.a29610af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/linkbio/static/js/2.a29610af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c2a9ffea87e3a3e529",
+    "url": "/linkbio/static/js/main.05476046.chunk.js"
+  },
+  {
+    "revision": "2b3731f804a85d80f014",
+    "url": "/linkbio/static/js/runtime-main.7b92597d.js"
+  }
+]);
