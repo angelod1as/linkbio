9d331de377bf8daf4fcb3195eb3ae48779eceb21
Angelo Dias <oiangelodias@gmail.com>
2020-08-13 13:26:52 -0300

    testing favicon

--- src/pages/Default/components/Export/components/components/head.ts
+++ src/pages/Default/components/Export/components/components/head.ts
@@ -11,17 +11,22 @@ const head = (header: ILists['header']['list']) => {
   <meta name="description" content="${header.name} list of links" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+  <link rel="apple-touch-icon" sizes="180x180" href="https://angelod1as.github.io/linkbio/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="https://angelod1as.github.io/linkbio/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="https://angelod1as.github.io/linkbio/favicon-16x16.png">
+  <link rel="manifest" href="https://angelod1as.github.io/linkbio/site.webmanifest">
+
   <!-- Twitter Card data -->
   <meta name="twitter:card" content="summary">
   <meta name="twitter:title" content="${header.name} - Linkbio">
   <meta name="twitter:description" content="${header.name} list of links">
   <!-- Twitter Summary card images must be at least 120x120px -->
-  <meta name="twitter:image" content="http://www.example.com/image.jpg">
+  <meta name="twitter:image" content="http://euro-travel-example.com/social.jpg">
 
   <!-- Open Graph data -->
   <meta property="og:title" content="${header.name} - Linkbio" />
   <meta property="og:type" content="article" />
-  <meta property="og:image" content="http://example.com/image.jpg" />
+  <meta property="og:image" content="http://euro-travel-example.com/social.jpg" />
   <meta property="og:description" content="${header.name} list of links" />
   <meta property="og:site_name" content="${header.name} - Linkbio" />
 
