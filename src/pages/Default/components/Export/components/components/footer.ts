f8a5ae24c58c8378edf6222fb9c168c35ed15038
Angelo Dias <oiangelodias@gmail.com>
2021-04-05 13:05:06 -0300

    update footer

--- src/pages/Default/components/Export/components/components/footer.ts
+++ src/pages/Default/components/Export/components/components/footer.ts
@@ -1,6 +1,6 @@
 const footer = () => {
   return `
-    <footer class="footer"><p>Developed by <a href="http://angelodias.com.br" rel="noopener noreferrer" target="_blank">Angelo Dias</a>. Contributions or feedback through <a href="http://angelodias.com.br" rel="noopener noreferrer" target="_blank">GitHub</a>. Make your own list <a href="http://angelodias.com.br" rel="noopener noreferrer" target="_blank">here</a>.</p></footer>
+    <footer class="footer"><p>Developed by <a href="http://angelodias.com.br" rel="noopener noreferrer" target="_blank">Angelo Dias</a>. Contributions or feedback through <a href="https://github.com/angelod1as/linkbio" rel="noopener noreferrer" target="_blank">GitHub</a>. Make your own list <a href="https://angelod1as.github.io/linkbio" rel="noopener noreferrer" target="_blank">here</a>.</p></footer>
   `;
 };
 
